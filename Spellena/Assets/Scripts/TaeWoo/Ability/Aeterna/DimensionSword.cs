bba1468c12e751bee6bfe1d14fef8e05f96af149
rlaoxdn <rlaxodn0215@naver.com>
2023-10-13 14:58:58 +0900

    20231013 - Aeterna Basic Attack

--- Spellena/Assets/Scripts/TaeWoo/Ability/Aeterna/DimensionSword.cs
+++ Spellena/Assets/Scripts/TaeWoo/Ability/Aeterna/DimensionSword.cs
@@ -9,17 +9,22 @@ namespace Player
         private Aeterna Player;
         private Animator animator;
         private GameObject Sword;
+        private int NearDamage = 80;
+        private int FarDamage = 120;
+
         public DimensionSword(Aeterna player)
         {
             Player = player;
             animator = player.GetComponent<Animator>();
-            //Sword = player.DimensionSword;
+            Sword = player.DimensionSword;
         }
         public void Execution()
         {
             //공격 애니매이션 사용, 칼 활성화
             //만일 타격이 없다면 원거리 공격
             animator.SetTrigger("BasicAttack");
+            Sword.GetComponent<BoxCollider>().enabled = true;
+
             Debug.Log("DimensionSword!");
             
         }
