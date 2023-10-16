8811513a4581eaae1183cd676078410a7dc9b648
cksdud624 <cksdud624@naver.com>
2023-10-16 17:26:09 +0900

    20231016 - 엘리멘탈 오더

--- Spellena/Assets/Scripts/ChanYoung/Character/ElementalOrder.cs
+++ Spellena/Assets/Scripts/ChanYoung/Character/ElementalOrder.cs
@@ -1,24 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Player
 {
     public class ElementalOrder : Character
     {
+        public CharacterData elementalOrderData;
+
+        [HideInInspector]
+        public int skillButton = -1;
+        [HideInInspector]
+        public float[] skillTimer;
+
+        //스킬 순서 11, 12, 13, 22, 23, 33 총 6개
+        List<int> commands = new List<int>();
+        bool isReadyToUseSkill = false;
+
+
+        
         protected override void Start()
         {
             base.Start();
+            Initialize();
         }
 
         protected override void Update()
         {
             base.Update();
+            PlayerSkillInput();
         }
 
         protected override void FixedUpdate()
         {
             base.FixedUpdate();
+        }
+
+        private void Initialize()
+        {
+            RagnaEdge ragnaEdge = this.gameObject.AddComponent<RagnaEdge>();
+            Skills["RagnaEdge"] = ragnaEdge;
+
+            skillTimer = new float[Skills.Count];
+
+            for(int i = 0; i < Skills.Count; i++)
+            {
+                skillTimer[i] = -1;
+            }
+
+            Hp = elementalOrderData.Hp;
+            moveSpeed = elementalOrderData.moveSpeed;
+            jumpHeight = elementalOrderData.jumpHeight;
+        }
+
+        void OnSkill1()
+        {
+            if(commands.Count < 2)
+            {
+                commands.Add(1);
+            }
+        }
+
+        void OnSkill2()
+        {
+            if (commands.Count < 2)
+            {
+                commands.Add(2);
+            }
+        }
+
+        void OnSkill3()
+        {
+            if (commands.Count < 2)
+            {
+                commands.Add(3);
+            }
+        }
+
+        void OnSkill4()
+        {
+            if(commands.Count >= 2)
+            {
+                isReadyToUseSkill = true;
+            }
+        }
+
+        protected void PlayerSkillInput()
+        {
+            if(isReadyToUseSkill == true)
+            {
+
+            }
         }
     }
 }
