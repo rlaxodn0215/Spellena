ea90fffe6cd8fb58b0cb58382cb10bd2b081c327
cksdud624 <cksdud624@naver.com>
2023-10-16 15:01:02 +0900

    20231016 - 엘리멘탈 오더 클래스 작성

---
+++ Spellena/Assets/Scripts/ChanYoung/Character/ElementalOrder.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class ElementalOrder : Charactor
+    {
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+        }
+
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+        }
+    }
+}
