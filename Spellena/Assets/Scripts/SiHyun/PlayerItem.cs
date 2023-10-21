18fd5ba5e7c593fa9f592f4e720c0b9d8f3c032a
Noh <56387273+NohZZang@users.noreply.github.com>
2023-10-22 01:32:09 +0900

    20231021 - 로비 시스템 수정

--- Spellena/Assets/Scripts/SiHyun/PlayerItem.cs
+++ Spellena/Assets/Scripts/SiHyun/PlayerItem.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class PlayerItem : MonoBehaviour
 {
+    public Text playerName;
+
+    Image backgroundImage;
+    public Color highlightColor;
+    public GameObject leftArrowButton;
+    public GameObject rightArrowButton;
+
+    public void SetPlayerInfo(Photon.Realtime.Player _player)
+    {
+        playerName.text = _player.NickName;
+    }
     // Start is called before the first frame update
     void Start()
     {
