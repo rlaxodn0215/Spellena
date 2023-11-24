07f5c217a552ebea37bb355ac0b8cac2ab3b151d
NohZZang <shtlgus0915@gmail.com>
2023-11-24 20:12:40 +0900

    20231124 - 커스텀 로비 UI 및 기능 수정

--- Spellena/Assets/Scripts/SiHyun/RoomItem.cs
+++ Spellena/Assets/Scripts/SiHyun/RoomItem.cs
@@ -6,6 +6,14 @@ using UnityEngine.UI;
 public class RoomItem : MonoBehaviour
 {
     public Text roomName;
+    public Text masterPlayerName;
+    public Text entryStatusText;
+    public Text playerCountText;
+    public Image entryIcon;
+    public GameObject blackOut;
+    public Sprite admissionPossible;
+    public Sprite entryNotPossible;
+    public int maxChar = 10;
     LobbyManager manager;
 
     private void Start()
@@ -13,13 +21,33 @@ public class RoomItem : MonoBehaviour
         manager = FindObjectOfType<LobbyManager>();
     }
 
-    public void SetRoomName(string _roomName)
+    public void SetRoomInfo(string _roomName, int _playerCount, int _maxPlayer, string _masterClientName,
+        string _gameState)
     {
-        roomName.text = _roomName;
+        if(_roomName.Length > maxChar)
+        {
+            roomName.text = _roomName.Substring(0, maxChar) + "...";
+        }
+        playerCountText.text = $"{_playerCount} / {_maxPlayer}";
+        masterPlayerName.text = _masterClientName;
+
+        if(_gameState == "InGame")
+        {
+            entryStatusText.text = "입장 불가";
+            blackOut.SetActive(true);
+            entryIcon.sprite = entryNotPossible;
+        }
+        else if(_gameState == "Waiting")
+        {
+            entryStatusText.text = "입장 가능";
+            blackOut.SetActive(false);
+            entryIcon.sprite = admissionPossible;
+        }
     }
 
     public void OnClickItem()
     {
+        Debug.Log(roomName.text + " : 방 입장");
         manager.JoinRoom(roomName.text);
     }
 
