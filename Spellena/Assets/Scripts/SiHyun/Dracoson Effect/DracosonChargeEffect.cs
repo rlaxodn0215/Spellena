081681ca20d3e0db6778ade277c84103f1b695cd
NohZZang <shtlgus0915@gmail.com>
2023-11-30 18:23:12 +0900

    20231130 - 드라코손 애니메이션 작업 및 평타 스킬 이펙트 적용

---
+++ Spellena/Assets/Scripts/SiHyun/Dracoson Effect/DracosonChargeEffect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DracosonChargeEffect : MonoBehaviour
+{
+    public Transform staffTop;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.position = new Vector3(staffTop.position.x, staffTop.position.y, staffTop.position.z);
+    }
+}
